877a32108566dd9a0948f8c70dee443ee6d282e1
Tyler Holmes <tylerdanielholmes@gmail.com>
2016-12-28 16:58:44 -0600

    tweaking build scripts for the hello_world demo to be used in the main project

--- firmware/MotorController/MKL03Z/MKL03Z4_drivers/boards/frdmkl03z/demo_apps/hello_world/armgcc/build_all.sh
+++ firmware/MotorController/MKL03Z/MKL03Z4_drivers/boards/frdmkl03z/demo_apps/hello_world/armgcc/build_all.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
-cmake -DCMAKE_TOOLCHAIN_FILE="../../../../../tools/cmake_toolchain_files/armgcc.cmake" -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug  .
+echo
+echo "---------------------- DEBUG BUILD -------------------------"
+echo
+cmake -DCMAKE_TOOLCHAIN_FILE="../../../../../tools/cmake_toolchain_files/armgcc.cmake" -G "Unix Makefiles" -Wno-deprecated -DCMAKE_BUILD_TYPE=Debug  .
 make -j4
-cmake -DCMAKE_TOOLCHAIN_FILE="../../../../../tools/cmake_toolchain_files/armgcc.cmake" -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release  .
+echo
+echo "--------------------- RELEASE BUILD ------------------------"
+echo
+cmake -DCMAKE_TOOLCHAIN_FILE="../../../../../tools/cmake_toolchain_files/armgcc.cmake" -G "Unix Makefiles" -Wno-deprecated -DCMAKE_BUILD_TYPE=Release  .
 make -j4
