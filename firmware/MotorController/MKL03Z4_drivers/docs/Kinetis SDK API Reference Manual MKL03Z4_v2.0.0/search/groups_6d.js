bd980f19c4f9f8b0e4f63d6275104b298a455580
Tyler Holmes <tylerdanielholmes@gmail.com>
2017-01-02 18:25:27 -0600

    removing all of the HTML documentation because it exists in PDF form

--- firmware/MotorController/MKL03Z4_drivers/docs/Kinetis SDK API Reference Manual MKL03Z4_v2.0.0/search/groups_6d.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['multipurpose_20clock_20generator_20lite_20_28mcglite_29',['Multipurpose Clock Generator Lite (MCGLITE)',['../group__mcglite.html',1,'']]]
-];
--- firmware/MotorController/MKL03Z4_drivers/docs/Kinetis SDK API Reference Manual MKL03Z4_v2.0.0/search/groups_78.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['xbar_3a_20inter_2dperipheral_20crossbar_20switch',['XBAR: Inter-Peripheral Crossbar Switch',['../group__xbar.html',1,'']]]
-];
